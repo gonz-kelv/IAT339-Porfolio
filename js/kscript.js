c0b6af812592cd51d42403f770410a5df23d4235
gonz-kelv <kelv.gonzales@gmail.com>
2015-11-17 15:24:22 -0800

    Nav bar working
    content animation not complete

--- js/kscript.js
+++ js/kscript.js
@@ -1,34 +1,58 @@
+var navOn = false;
+
 $(document).ready( function() {
 	//console.log("yes");
 	
+	// Hide the contents
+	// $('.Content-Me').hide();
+	// $('.Content-Resume').hide();
+	// $('.Content-Work').hide();
+	// $('.Content-Contact').hide();
+	
 	$('.Nav.Top').on('click', function(){
+		navOn  = true;
 		console.log('clicking');
 		
 		$('.Nav.Top').animate({
-			'margin-top':0,
+			'margin-top':'1%',
+			'margin-bottom':'29%',
 			'top': 0
 		}, 1000);
 		
 		$('.Nav.Left').animate({
 			'margin-left':0,
-			'margin-top':'35%',
 			'left':0
 		}, 1000);
 		
 		$('.Nav.Right').animate({
 			'margin-right':0,
-			'margin-top':'35%',
 			'right':0
 		}, 1000);
 		
 		$('.Nav.Bottom').animate({
 			'bottom':0,
-
-			
-		},1000);
+			'margin-bottom':0,
+			'margin-top': '60%'		
+		},1000).attr('position', 'fixed');
 		
+		// $('.Content-Me').show().animate({
+		// 	'padding-top': '30%',
+		// 	'padding-bottom': '10%' 
+		// },1000);
 		
+	});
+	
+	$('.Nav.Left').on('click', function(){
 		
-	})
+		// $('.Content-Me').show().animate({
+		// 	'padding-top': 0,
+		// 	'margin-left': '60%' 
+		// },1000).attr('position', 'absolute');
+		
+		// $('.Content-Resume').show().animate({
+		// 	'padding-top': '30%',
+		// 	'padding-bottom': '10%' 
+		// },1000);
+	});
 	
 });
